e9e57dcd19945c4dadb7e885c3bd2424402e5840
aziz-shoko <azizbek.shokosimov@gmail.com>
2025-07-21 12:15:08 -0500

    added logger and metrics and their mocks

---
+++ pkg/gofr/datasource/kv-store/dynamo/metrics.go
@@ -0,0 +1,10 @@
+package dynamo
+
+import (
+	"context"
+)
+
+type Metrics interface {
+	NewHistogram(name, desc string, buckets ...float64)
+	RecordHistogram(ctx context.Context, name string, value float64, labels ...string)
+}
